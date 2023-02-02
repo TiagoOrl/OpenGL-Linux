f32239f66267e55568c7dcf913abef5e32ae7624
Tiago Orlando <tiago_orlando@yahoo.com>
2023-02-01 23:09:42 -0300

    feat: added pyramid 3d rendering

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
+#include <stb/stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "classes/shader.hpp"
 #include "classes/VBO.hpp"
 #include "classes/VAO.hpp"
 #include "classes/EBO.hpp"
 #include "classes/Texture.hpp"
-#include "stb/stb_image.h"
 
 // install:
 // libgl1-mesa-dev
@@ -20,6 +23,7 @@
 
 
 int main() {
+    int width = 800, height = 800;
     glfwInit();
 
     // tell GLFW what version of OpenGL we are using it = 3.3
@@ -44,8 +48,28 @@ int main() {
         0, 3, 2  // lower triangle
     };
 
+    GLfloat pyramidVerts[] =
+    { //     COORDINATES     /        COLORS      /   TexCoord  //
+        -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+        -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+         0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+         0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+         0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
+    };
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    // Indices for vertices order
+    GLuint pyramidIndexes[] =
+    {
+        0, 1, 2,
+        0, 2, 3,
+        0, 1, 4,
+        1, 2, 4,
+        2, 3, 4,
+        3, 0, 4
+    };
+
+
+    GLFWwindow *window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
 
     if (window == NULL) {
         std::cout << "Failed to create GLFW window\n";
@@ -60,7 +84,7 @@ int main() {
     gladLoadGL();
 
     // specify render viewport
-    glViewport(0, 0, 600, 600);
+    glViewport(0, 0, width, height);
 
     // creates the shaders vertex and fragment
     Shader shaderProgram("shaders/default.vert", "shaders/default.frag");
@@ -70,8 +94,8 @@ int main() {
     VAO1.Bind();
 
     // vertex buffer and elements buffer objects
-    VBO VBO1(vertices, sizeof(vertices));
-    EBO EBO1(indexes, sizeof(indexes));
+    VBO VBO1(pyramidVerts, sizeof(pyramidVerts));
+    EBO EBO1(pyramidIndexes, sizeof(pyramidIndexes));
     
 
     // link the VBO attributes such as coordinates, colors and textures
@@ -87,17 +111,50 @@ int main() {
     Texture brickWall("textures/brick_wall.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
     brickWall.texUnit(shaderProgram, "tex0", 0);
 
+    float rotation = 0.0f;
+    double prevTime = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window)) 
     {
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         shaderProgram.Activate();
+
+        double currentTime = glfwGetTime();
+        if (currentTime - prevTime >= 1.0f / 60.0f) {
+            rotation += 0.5f;
+            prevTime = currentTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.4f));                                   
+                                                // y down -0.5, z forward -2.0
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glUniform1f(uniID, 0.5f);
         // Binds texture so that is appears in rendering
 		brickWall.Bind();
         VAO1.Bind();
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //draw img
+        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glDrawElements(GL_TRIANGLES, sizeof(pyramidIndexes)/sizeof(int32_t), GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window); 
 
         // handle GLFW events
