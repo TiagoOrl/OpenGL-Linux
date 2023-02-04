c10f69c453603daa9f9507460de8cd3519c75b18
Tiago Orlando <tiago_orlando@yahoo.com>
2023-02-04 20:07:16 -0300

    feat: adding lighting

--- main.cpp
+++ main.cpp
@@ -69,6 +69,34 @@ int main() {
         3, 0, 4
     };
 
+    GLfloat lightVertices[] =
+    { //     COORDINATES     //
+        -0.1f, -0.1f,  0.1f,
+        -0.1f, -0.1f, -0.1f,
+        0.1f, -0.1f, -0.1f,
+        0.1f, -0.1f,  0.1f,
+        -0.1f,  0.1f,  0.1f,
+        -0.1f,  0.1f, -0.1f,
+        0.1f,  0.1f, -0.1f,
+        0.1f,  0.1f,  0.1f
+    };
+
+    GLuint lightIndices[] =
+    {
+        0, 1, 2,
+        0, 2, 3,
+        0, 4, 7,
+        0, 7, 3,
+        3, 7, 6,
+        3, 6, 2,
+        2, 6, 5,
+        2, 5, 1,
+        1, 5, 4,
+        1, 4, 0,
+        4, 5, 6,
+        4, 6, 7
+    };
+
 
     GLFWwindow *window = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
 
@@ -107,6 +135,39 @@ int main() {
     VBO1.Unbind();
     EBO1.Unbind();
 
+    // Shader for light cube
+	Shader lightShader("shaders/light.vert", "shaders/light.frag");
+	// Generates Vertex Array Object and binds it
+	VAO lightVAO;
+	lightVAO.Bind();
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
+	// Links VBO attributes such as coordinates and colors to VAO
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+	// Unbind all to prevent accidentally modifying them
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
+
+
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramidModel = glm::mat4(1.0f);
+	pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	shaderProgram.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramidModel));
+
+
     Texture brickWall("textures/brick_wall.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
     brickWall.texUnit(shaderProgram, "tex0", 0);
 
@@ -119,10 +180,12 @@ int main() {
     {
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        shaderProgram.Activate();
 
         camera.Inputs(window);
         camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+        shaderProgram.Activate();
+
         camera.Matrix(shaderProgram, "camMatrix");
 
         // Binds texture so that is appears in rendering
@@ -133,6 +196,12 @@ int main() {
         // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         glDrawElements(GL_TRIANGLES, sizeof(pyramidIndexes)/sizeof(int32_t), GL_UNSIGNED_INT, 0);
+
+        lightShader.Activate();
+        camera.Matrix(lightShader, "camMatrix");
+        lightVAO.Bind();
+        glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window); 
 
         // handle GLFW events
